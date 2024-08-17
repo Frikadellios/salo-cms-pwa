ec87e2572a1d69988b01ff791ef3e0217e55d216
Hrihorii Ilin <devopsick@pm.me>
2024-08-17 15:13:10 +0300

    add inlang

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -1,12 +1,12 @@
 interface Locale {
 	[key: string]: string
 }
-import de from '@/locales/de.json'
-import en from '@/locales/en.json'
-import es from '@/locales/es.json'
-import fr from '@/locales/fr.json'
-import hr from '@/locales/hr.json'
-import nl from '@/locales/nl.json'
+import de from './locales/de.json'
+import en from './locales/en.json'
+import es from './locales/es.json'
+import fr from './locales/fr.json'
+import hr from './locales/hr.json'
+import nl from './locales/nl.json'
 
 const lang = import.meta.env.WEBSITE_LANGUAGE
 export const t = (field: string): string => {
